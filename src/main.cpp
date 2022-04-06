496e8892bcc3e6473852954ad6d1aadc33076279
Thomas Briggs <thomas.briggs333@gmail.com>
2022-04-06 17:13:01 +0100

    Added flag to set number of words

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "file.h"
 #include "typer.h"
 #include <iostream>
+#include <string>
 
 int main(int argc, char const* argv[])
 {
@@ -8,6 +9,15 @@ int main(int argc, char const* argv[])
 
     if (argc == 1)
         words = getRandomWords("/home/thomas/terminal-typer/top_100.txt", 50);
+    else if (argc == 2) {
+        int num = std::stoi(std::string(argv[1]));
+        if (num < 0)
+            std::cerr << "Please enter a number above 0!\n";
+        else if (num > 100)
+            words = getRandomWords("/home/thomas/terminal-typer/top_1000.txt", num);
+        else
+            words = getRandomWords("/home/thomas/terminal-typer/top_100.txt", num);
+    }
 
     typer(words);
     return 0;
