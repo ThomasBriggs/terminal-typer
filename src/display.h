0ded9a5bf4cc4c93943dc612599bcb4a912e6014
Thomas Briggs <thomas.rbiggs333@gmail.com>
2022-04-07 07:01:06 +0100

    Uses cont pointers

--- src/display.h
+++ src/display.h
@@ -8,10 +8,10 @@
 #include <string>
 #include <vector>
 
-void displayWords(WINDOW* win, std::vector<std::string> words, int currentWord, std::vector<std::string> inputWords);
-void displayInput(WINDOW* win, const std::string& s);
-void clearInput(WINDOW* win);
-void drawInput(WINDOW* win);
+void displayWords(WINDOW* const win, const std::vector<std::string>& words, int currentWord, const std::vector<std::string>& inputWords);
+void displayInput(WINDOW* const win, const std::string& s);
+void clearInput(WINDOW* const win);
+void drawInput(WINDOW* const win);
 WINDOW* getInputWin();
 WINDOW* getPromptWin();
 void displayResults(const std::chrono::milliseconds& duration, const size_t& charsTyped, const size_t& correctCharsTyped, const size_t& numWords,
