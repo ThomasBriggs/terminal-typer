f41d4d9807f5c9265dccd75d0ed92a5a4edfcd9c
Thomas Briggs <thomas.rbiggs333@gmail.com>
2022-04-07 13:10:28 +0100

    Added filesystem and errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,33 @@
 #include "file.h"
 #include "typer.h"
+#include <filesystem>
 #include <iostream>
 #include <string>
 
 int main(int argc, char const* argv[])
 {
+    std::filesystem::path root = "../res";
     std::vector<std::string> words;
 
     if (argc == 1)
-        words = getRandomWords("/home/thomas/terminal-typer/top_100.txt", 50);
+        words = getRandomWords(root.append("top_100.txt"), 50);
     else if (argc == 2) {
-        int num = std::stoi(std::string(argv[1]));
-        if (num < 1) {
-            std::cerr << "Please enter a number above 0!\n";
+        int num;
+        try {
+            num = std::stoi(std::string(argv[1]));
+        } catch (std::invalid_argument& e) {
+            std::cerr << "Must be a integer!\n";
             exit(1);
-        } else if (num > 101)
-            words = getRandomWords("/home/thomas/terminal-typer/top_1000.txt", num);
-        else
-            words = getRandomWords("/home/thomas/terminal-typer/top_100.txt", num);
+        }
+
+        if (num < 1) {
+            std::cerr << "Integet must be > 0!\n";
+            exit(1);
+        } else
+            words = getRandomWords(root.append("top_100.txt"), num);
+    } else {
+        std::cerr << "RTFM!\n";
+        exit(1);
     }
 
     typer(words);
