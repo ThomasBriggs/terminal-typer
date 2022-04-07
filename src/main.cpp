837b54f397c0033c895b37b74d19b4486c5fc9a7
Thomas Briggs <thomas.rbiggs333@gmail.com>
2022-04-07 18:33:05 +0100

    Added options

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "cxxopts.hpp"
 #include "file.h"
 #include "typer.h"
 #include <filesystem>
@@ -8,27 +9,42 @@ int main(int argc, char const* argv[])
 {
     std::filesystem::path root = "../res";
     std::vector<std::string> words;
+    bool mode_set = false;
+
+    cxxopts::Options options("Terminal-typer", "A terminal typing game");
+    options.add_options()(
+        "q, quote", "Type a random quote", cxxopts::value<std::string>()->implicit_value(""))(
+        "w, words", "Type 50 random words from top 100 words, or --words=X where X is the number of words", cxxopts::value<int>()->implicit_value("50")->default_value("50"))(
+        "words-list", "The word list to choose random words from, if used with --quote, this will have no effect", cxxopts::value<std::string>()->default_value("top_100.txt"))(
+        "h, help", "Print usage");
+
+    auto results = options.parse(argc, argv);
 
     if (argc == 1)
-        words = getRandomWords(root.append("top_100.txt"), 50);
-    else if (argc == 2) {
-        int num;
-        try {
-            num = std::stoi(std::string(argv[1]));
-        } catch (std::invalid_argument& e) {
-            std::cerr << "Must be a integer!\n";
-            exit(1);
-        }
+        words = getRandomWords(root.append(results["words-list"].as<std::string>()), results["words"].as<int>());
 
-        if (num < 1) {
-            std::cerr << "Integet must be > 0!\n";
-            exit(1);
-        } else
-            words = getRandomWords(root.append("top_100.txt"), num);
-    } else {
-        std::cerr << "RTFM!\n";
-        exit(1);
+    if (results.count("help")) {
+        std::cout << options.help() << '\n';
+        exit(0);
     }
+
+    if (results.count("quote")) {
+        mode_set = true;
+        const std::string& quotePath = results["quote"].as<std::string>();
+        if (quotePath == "")
+            words = getAllWords(root.append("aladin.txt"));
+        else {
+            words = getAllWords(quotePath);
+        }
+    }
+
+    if (results.count("words")) {
+        mode_set = true;
+        words = getRandomWords(root.append(results["words-list"].as<std::string>()), results["words"].as<int>());
+    }
+
+    if (!mode_set)
+        words = getRandomWords(root.append(results["words-list"].as<std::string>()), results["words"].as<int>());
 
     typer(words);
     return 0;
