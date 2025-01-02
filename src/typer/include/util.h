255f661502c68e744048a5d898bddf9fe46bf716
Thomas Briggs <thomas.briggs333@gmail.com>
2025-01-02 16:13:58 +0000

    Revert "Add words"
    This reverts commit 42ae1dc9921237bc439f3374748b5d0a63a4faf2.

--- src/typer/include/util.h
+++ src/typer/include/util.h
@@ -6,4 +6,3 @@
 float getAccuracy(const std::vector<std::string>& input, const std::vector<std::string>& goal);
 bool isCharCorrect(const std::string& currentWord, const std::string& inputWord);
 bool isInputCorrect(const std::string& word, const std::string& input);
-bool isCharCorrectAtPos(const std::string &currentWord, const std::string &inputWord, const int pos);
